3d1caa3f5756fdfe2f2b95665066a5cc8a124105
Kalyan Sunkara <63612268+Kalyan-Sunkara@users.noreply.github.com>
2021-04-21 18:41:09 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -14,10 +14,36 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRC = 0xFF;
+	PORTC = 0x00;
     /* Insert your solution below */
+//	unsigned char total = 0;
+	unsigned char tmpA = 0;
     while (1) {
-
-    }
+	tmpA = PINA & 0x0F;
+	if(tmpA == 0){
+		PORTC = 0x40;
+	}
+	else if(tmpA < 3){
+		PORTC = 0x60;
+	}
+	else if(tmpA < 5){
+		PORTC = 0x70;
+	}
+	else if(tmpA < 7){
+                PORTC = 0x38;
+        }
+	else if(tmpA < 10){
+		PORTC = 0x3C;
+	}
+	else if(tmpA < 13){
+		PORTC = 0x3E;
+	}
+	else{
+		PORTC = 0x3F;
+	}		
+}	
     return 1;
 }
