c87f6294b13ad68f6d4ed620faebd9e62cde2ab6
Kalyan Sunkara <63612268+Kalyan-Sunkara@users.noreply.github.com>
2021-04-21 19:16:34 -0700

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -1,34 +1,26 @@
-# Array of tests to run (in order)
-# Each test contains
-#   description - 
-#   steps - A list of steps to perform, each step can have
-#       inputs - A list of tuples for the inputs to apply at that step
-#       *time - The time (in ms) to wait before continuing to the next step 
-#           and before checking expected values for this step. The time should be a multiple of
-#           the period of the system
-#       *iterations - The number of clock ticks to wait (periods)
-#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
-#           If this value is incorrect the test will fail early before completing.
-#       * only one of these should be used
-#   expected - The expected output (as a list of tuples) at the end of this test
-# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
-# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
-# altered in between executions (unless preconditions are used).
-tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
-    'expected': [('PORT',<val>)],
+tests = [ {'description': 'Level 1-2',
+    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x3F)],
     },
-    {'description': 'This test will run second.',
-    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
-        {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
-        {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
-        {'inputs': [('PIN',<val>)], 'time': 600}, ],
-    'expected': [('PORT',<val>)],
+    {'description': 'Level 3-4',
+    'steps': [ {'inputs': [('PINA', 0x04)],'iterations': 5}],
+    'expected': [('PORTC',0x3E)],
+    }, 
+    {'description': 'Level 5-6',
+    'steps': [ {'inputs': [('PINA', 0x05)],'iterations': 5}],
+    'expected': [('PORTC',0x3C)],
+    },
+    {'description': 'Level 7-8-9',
+    'steps': [ {'inputs': [('PINA', 0x09)],'iterations': 5}],
+    'expected': [('PORTC',0x38)],
+    },
+    {'description': 'Level 10-11-12',
+    'steps': [ {'inputs': [('PINA', 0x0B)],'iterations': 5}],
+    'expected': [('PORTC',0x70)],
+    },
+    {'description': 'Level 13-14-15',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x60)],
     },
     ]
-
-# Optionally you can add a set of "watch" variables these need to be global or static and may need
-# to be scoped at the function level (for static variables) if there are naming conflicts. The 
-# variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['<function>::<static-var>','PORTB']
-
+#watch = ['<function>::<static-var>','PORTB']
